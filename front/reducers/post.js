d6f4bd0d034d86486eba9fef4117a6dea5dd25f6
5o-hyun <94782559+5o-hyun@users.noreply.github.com>
2023-06-12 12:15:02 +0900

    Feat: faker dummy data

--- front/reducers/post.js
+++ front/reducers/post.js
@@ -1,5 +1,7 @@
 import { produce } from "immer";
 import shortId from "shortid";
+import { faker } from "@faker-js/faker";
+faker.seed(123);
 
 export const initialState = {
   mainPosts: [
@@ -60,6 +62,33 @@ export const initialState = {
   addCommentDone: false,
   addCommentError: null,
 };
+
+initialState.mainPosts = initialState.mainPosts.concat(
+  Array(28)
+    .fill()
+    .map(() => ({
+      id: shortId.generate(),
+      User: {
+        id: shortId.generate(),
+        nickname: faker.person.firstName(),
+      },
+      content: faker.lorem.paragraph(),
+      Images: [
+        {
+          src: faker.image.url(),
+        },
+      ],
+      Comments: [
+        {
+          User: {
+            id: shortId.generate(),
+            nickname: faker.person.firstName(),
+          },
+          content: faker.lorem.sentence(),
+        },
+      ],
+    }))
+);
 
 export const ADD_POST_REQUEST = "ADD_POST_REQUEST";
 export const ADD_POST_SUCCESS = "ADD_POST_SUCCESS";
